037d02b77473ca6d4a6944c96cf06ffbf8c831dd
Enes Pala <enespalaio7@gmail.com>
2026-07-08 22:02:40 +0300

    feat: create ve get notifications yazildi

---
+++ services/notification_service.go
@@ -0,0 +1,23 @@
+package services
+
+import (
+	"kan-uygulamasi/database"
+	"kan-uygulamasi/models"
+)
+
+func CreateNotification(userID uint, title, message string) error {
+	notification := models.Notification{
+		UserID:  userID,
+		Title:   title,
+		Message: message,
+	}
+
+	return database.DB.Create(&notification).Error
+}
+
+func GetMyNotifications(userID uint) ([]models.Notification, error) {
+	var notifications []models.Notification
+
+	err := database.DB.Where("user_id = ?", userID).Order("created_at desc").Find(&notifications).Error
+	return notifications, err
+}
