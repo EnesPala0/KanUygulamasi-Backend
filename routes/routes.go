aa1fb4bd201455a26ff1f272436516c6d7dec757
Enes Pala <enespalaio7@gmail.com>
2026-07-13 23:06:36 +0300

    feat(api): CORS ayarları eklendi ve MVP rotaları tamamlandı

--- routes/routes.go
+++ routes/routes.go
@@ -3,12 +3,23 @@ package routes
 import (
 	"kan-uygulamasi/handlers"
 	"kan-uygulamasi/middlewares"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(router *gin.Engine) {
 	api := router.Group("/api")
+
+	api.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1:5500", "http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Accept"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	{
 		// --- HERKESE AÇIK ROTALAR (Token gerektirmez) ---
 		api.POST("/users", handlers.CreateUser)
