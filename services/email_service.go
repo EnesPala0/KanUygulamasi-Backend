a47ee87e93cce43d5c7b782497b0339a12bc3b61
Enes Pala <enespalaio7@gmail.com>
2026-08-10 16:52:50 +0300

    connect resend api

---
+++ services/email_service.go
@@ -0,0 +1,34 @@
+package services
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/resend/resend-go/v2"
+)
+
+// SendEmail, resend API'sini kullanarak e-posta gönderir
+func SendEmail(to string, subject string, htmlContent string) error {
+	apiKey := os.Getenv("RESEND_API_KEY")
+	if apiKey == "" {
+		return fmt.Errorf("RESEND_API_KEY environment variable is not set")
+	}
+
+	client := resend.NewClient(apiKey)
+
+	params := &resend.SendEmailRequest{
+		From:    "KanBağı App <onboarding@resend.dev>",
+		To:      []string{to}, // Şimdilik sadece kendi mailine gidecek
+		Subject: subject,
+		Html:    htmlContent,
+	}
+
+	sent, err := client.Emails.Send(params)
+	if err != nil {
+		return fmt.Errorf("mail gönderilemedi (Resend API Hatası): %v", err)
+	}
+
+	fmt.Printf("Mail başarıyla fırlatıldı! Resend ID: %s\n", sent.Id)
+	return nil
+
+}
