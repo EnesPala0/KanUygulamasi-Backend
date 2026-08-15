c4d5919c3c44b7b94b8e6ba9160c27ba1040fa32
Enes Pala <enespalaio7@gmail.com>
2026-08-15 17:32:07 +0300

    fix forgot password route

--- routes/routes.go
+++ routes/routes.go
@@ -36,7 +36,7 @@ func SetupRoutes(router *gin.Engine) {
 		api.GET("/users/:id", handlers.GetUserByID)
 		api.POST("/users/verify", handlers.VerifyUserOTP)
 		api.POST("/users/reset-password", handlers.ResetPassword)
-		api.POST("/api/users/forgot-password", handlers.ForgotPassword)
+		api.POST("/users/forgot-password", handlers.ForgotPassword)
 		// --- KORUMALI ROTALAR (Token gerekir) ---
 		// Bir alt grup oluşturup AuthMiddleware'i buraya bağlıyoruz
 		protected := api.Group("/")
